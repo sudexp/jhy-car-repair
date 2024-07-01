0a25c6fb7437817d1be47f74bd32f4907f91df44
Dmitry Sklyarov <dmitry.sklyarov@movya.fi>
2024-07-01 20:11:41 +0300

    required dependencies installed, project configured

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{html,svelte,js,ts}'],
+	theme: {
+		extend: {}
+	},
+	plugins: [require('daisyui')] // eslint-disable-line @typescript-eslint/no-require-imports
+};
