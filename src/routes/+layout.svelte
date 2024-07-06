03be5706d9131871810bc10bfd158c23b9a1bc5a
Dmitry Sklyarov <dmitry.sklyarov@movya.fi>
2024-07-06 14:44:54 +0300

    bgImage added

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,9 +3,10 @@
 	import '../app.css';
 	import Navbar from '$lib/components/navbar.svelte';
 	import Footer from '$lib/components/footer.svelte';
+	import bgImage from '$lib/jpg/bgImage.avif';
 </script>
 
-<div class="flex flex-col min-h-screen">
+<div class="flex flex-col min-h-screen" style="background-image: url({bgImage})">
 	<Navbar />
 	<main class="flex-grow">
 		<slot />
