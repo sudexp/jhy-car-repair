f3493b1886cf8c40fed8e86bef3f6c71006d139f
Dmitry Sklyarov <dmitry.sklyarov@movya.fi>
2024-07-07 12:15:39 +0300

    unnecessary margin removed

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -37,7 +37,7 @@
 				<Logo {isRootPath} isDrawer={true} />
 			</div>
 			<hr class="m-0" />
-			<ul class="px-0 py-2 m-0">
+			<ul class="px-0 py-2">
 				{#each navItems as item}
 					<Navlink href={item.href}>{item.name}</Navlink>
 				{/each}
