caf451852f942573eb3c0ce2dc207b6b69fb029b
Dmitry Sklyarov <dmitry.sklyarov@movya.fi>
2024-07-07 12:51:57 +0300

    error page created

---
+++ src/routes/+error.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<div class="h-full min-h-full flex-center">
+	<div class="flex-center flex-col">
+		<h2>
+			{$page.status}
+			{$page.error?.message}
+		</h2>
+		<p class="p2">Valitettavasti emme löydä tätä sivua.</p>
+		<a href="/" class="no-underline p1 text-secondary">Etusivulle</a>
+	</div>
+</div>
