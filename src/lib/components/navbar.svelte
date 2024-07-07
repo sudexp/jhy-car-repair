d361706c3d5ce58560d85cddca6c3baee251344d
Dmitry Sklyarov <dmitry.sklyarov@movya.fi>
2024-07-07 11:43:12 +0300

    MenuIcon component

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -2,6 +2,7 @@
 	import { navItems } from '../utils';
 	import Logo from './logo.svelte';
 	import Navlink from './navlink.svelte';
+	import MenuIcon from '$lib/svg/menuIcon.svelte';
 
 	export let isRootPath: boolean;
 </script>
@@ -12,19 +13,7 @@
 		<div class="navbar w-full p-0">
 			<div class="flex-none sm:hidden">
 				<label for="my-drawer-3" aria-label="open sidebar" class="btn btn-square btn-ghost">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 24 24"
-						class="inline-block h-6 w-6 stroke-current"
-					>
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							stroke-width="2"
-							d="M4 6h16M4 12h16M4 18h16"
-						/>
-					</svg>
+					<MenuIcon />
 				</label>
 			</div>
 			<div class="hidden mx-2 flex-1 px-2 sm:block"><Logo {isRootPath} /></div>
