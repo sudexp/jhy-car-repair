47e49ac476084a23efa032df34bc0f7d3dd547de
Dmitry Sklyarov <dmitry.sklyarov@movya.fi>
2024-07-07 13:30:59 +0300

    phone icon created, content (without styling) added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,17 @@
-<p class="p1">Content here</p>
+<script lang="ts">
+	import PhoneSvg from '$lib/svg/phoneIcon.svelte';
+</script>
+
+<div class="text-neutral">
+	<h1 class="text-neutral">JYVÄSKYLÄN HUOLTOYKKÖSET</h1>
+	<p class="p2">Luottamuksellinen autonhuollon kumppanisi!</p>
+	<p class="p2">
+		Tervetuloa paremman autohuollon asiakkaaksi. Tarjoamme teille korkealaatuisia autonhuollon
+		palveluita, jotka perustuvat yli kymmenen vuoden kokemukseen ja ammattitaitoon. Tarkoituksemme
+		on auttaa teitä löytämään parhaat autonhuollon ratkaisut ja tarjota ne edullisesti.
+	</p>
+	<p class="p2">
+		Palvelemme ma–pe klo 8:00–16.30 osoitteessa <b>Kuormaajantie 6, 40320, Jyväskylä</b>.
+	</p>
+	<p class="p2">Soita meille: <PhoneSvg /> 040 123 4567</p>
+</div>
