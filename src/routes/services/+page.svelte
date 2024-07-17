1340b5b2652c8331390ce5331150f6b44470c8a8
Dmitry Sklyarov <dmitry.sklyarov@movya.fi>
2024-07-17 22:35:56 +0300

    page layout modified, service-icons created

--- src/routes/services/+page.svelte
+++ src/routes/services/+page.svelte
@@ -1,11 +1,21 @@
-<div class="space-y-4">
-	<h2>TÄYDEN PALVELUN AUTOHUOLTO KUSTANNUSTEHOKKAASTI</h2>
-	<p class="text-regular-3">
-		Autohuoltomme kattaa laajan valikoiman palveluita autosi kunnon ylläpitämiseksi. Tavoitteenamme
-		on tarjota luotettavaa ja tehokasta palvelua, kattamaan kaikki tarvittavat huollot ja
-		korjaukset, yksinkertaisista teknisistä huolloista raskaisiin korjaustoimenpiteisiin. Pyrimme
-		tekemään asiakaskokemuksestanne mukavan ja vaivattoman tarjoamalla laadukasta palvelua ja
-		yksilöllistä lähestymistapaa jokaiseen asiakkaaseen.
-	</p>
-	<p class="text-bold-3">Palvelumme sisältää:</p>
+<div class="space-y-6">
+	<div class="space-y-6">
+		<h2>TÄYDEN PALVELUN AUTOHUOLTO KUSTANNUSTEHOKKAASTI</h2>
+		<div class="space-y-4">
+			<p class="text-regular-3">
+				Autohuoltomme kattaa laajan valikoiman palveluita autosi kunnon ylläpitämiseksi.
+				Tavoitteenamme on tarjota luotettavaa ja tehokasta palvelua, kattamaan kaikki tarvittavat
+				huollot ja korjaukset, yksinkertaisista teknisistä huolloista raskaisiin
+				korjaustoimenpiteisiin. Pyrimme tekemään asiakaskokemuksestanne mukavan ja vaivattoman
+				tarjoamalla laadukasta palvelua ja yksilöllistä lähestymistapaa jokaiseen asiakkaaseen.
+			</p>
+			<p class="text-bold-3">Palvelumme sisältää:</p>
+		</div>
+	</div>
+	<div class="grid grid-cols-2 gap-6 sm:grid-cols-1">
+		<div>Qwert</div>
+		<div>Qwert</div>
+		<div>Qwert</div>
+		<div>Qwert</div>
+	</div>
 </div>
