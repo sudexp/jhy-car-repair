865cdfe670ee8515a6a3b0d170a89b6ece4e6014
Dmitry Sklyarov <dmitry.sklyarov@movya.fi>
2024-07-17 22:15:35 +0300

    page created

---
+++ src/routes/services/+page.svelte
@@ -0,0 +1,11 @@
+<div class="space-y-4">
+	<h2>TÄYDEN PALVELUN AUTOHUOLTO KUSTANNUSTEHOKKAASTI</h2>
+	<p class="text-regular-3">
+		Autohuoltomme kattaa laajan valikoiman palveluita autosi kunnon ylläpitämiseksi. Tavoitteenamme
+		on tarjota luotettavaa ja tehokasta palvelua, kattamaan kaikki tarvittavat huollot ja
+		korjaukset, yksinkertaisista teknisistä huolloista raskaisiin korjaustoimenpiteisiin. Pyrimme
+		tekemään asiakaskokemuksestanne mukavan ja vaivattoman tarjoamalla laadukasta palvelua ja
+		yksilöllistä lähestymistapaa jokaiseen asiakkaaseen.
+	</p>
+	<p class="text-bold-3">Palvelumme sisältää:</p>
+</div>
