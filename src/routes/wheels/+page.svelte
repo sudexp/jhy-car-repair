9366e31766ec492f6d051d20644dbc74cf752da5
Dmitry Sklyarov <dmitry.sklyarov@movya.fi>
2024-07-18 16:01:46 +0300

    page created

---
+++ src/routes/wheels/+page.svelte
@@ -0,0 +1,11 @@
+<div class="space-y-6">
+	<h2>RENGASMYYNTI JA RENGASHOTELLI</h2>
+	<div class="space-y-4">
+		<p class="text-bold-3">Kunnon renkaat on halpa henkivakuutus!</p>
+		<p class="text-regular-3">
+			Meiltä saat järkevän tarjouksen kunnon talvi- ja kesärenkaille. Tule käymään niin tarkistamme
+			veloituksetta renkaasi! Me säilytämme rengashotellissamme autosi renkaita sesongin ylitse kuin
+			omiamme. Rengashotellitarjouksemme pitää sisällään renkaiden pesun ja allevaihdon.
+		</p>
+	</div>
+</div>
