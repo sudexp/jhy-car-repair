75ef1ad71e73de570089e68b912a9fb5f5475c3f
Dmitry Sklyarov <dmitry.sklyarov@movya.fi>
2024-07-18 17:37:12 +0300

    page created

---
+++ src/routes/contacts/+page.svelte
@@ -0,0 +1,28 @@
+<div class="space-y-6">
+	<h2>YHTEYSTIEDOT</h2>
+	<div class="space-y-4">
+		<p class="text-bold-3">Kokemus ja ammattitaito jokaisessa yksityiskohdassa!</p>
+		<p class="text-regular-3">
+			Ylpeydellä esittelemme teille meidän kokeneen ja ammattitaitoisen henkilöstömme. Tavoitteemme
+			on tarjota teille korkealaatuisen autonhuollon palveluita, jotka perustuvat yli kymmenen
+			vuoden kokemukseen ja ammattitaitoon.
+		</p>
+		<p class="text-regular-3">
+			Huoltomme erottautuu korkealla koulutuksella ja Ammattitaidolla, minkä avulla voimme tarjota
+			teille aivan erityislaatuista palvelua. Työskentelymme on aina erityisen tarkkaa ja
+			huolellista, mikä takaa teille parhaan mahdollisen lopputuloksen.
+		</p>
+		<p class="text-regular-3">
+			Olemme avoinna arkisin klo 8:00–16.30 osoitteessa Kuormaajantie 6, 40320, Jyväskylä. Ota
+			yhteyttä meille puhelimitse <a href="tel:0452393591" class="link no-underline hover:underline"
+				>0452393591</a
+			> ja varaa aikaa.
+		</p>
+		<p class="text-bold-3">Meidän tiimimme:</p>
+
+		<p class="text-regular-3">
+			Löydä meidät helposti Seppälänkankaalta! Sijaintimme tekee meistä täydellisen valinnan sekä
+			koti- että työmatkailijoille, jotka etsivät korkealaatuisia autonhuollon palveluita läheltä.
+		</p>
+	</div>
+</div>
